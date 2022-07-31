fc1c316527c2d054c7bfbad200d79fca03ea3a4d
Josh Rae <joshuar2155@gmail.com>
2022-07-31 19:11:45 -0400

    url query

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-
-
 var city="";
 
 var searchCity = $("#search-city");
@@ -30,3 +28,53 @@ function displayWeather(event){
         currentWeather(city);
     }
 }
+function currentWeather(city){
+    
+    var queryURL= "" + city + "" + APIKey;
+    $.ajax({
+        url:queryURL,
+        method:"GET",
+    }).then(function(response){
+         
+         console.log(response);
+         
+         var weathericon= response.weather[0].icon;
+         var iconurl=""+weathericon +"";
+         
+         var date=new Date(response.dt*1000).toLocaleDateString();
+         
+         $(currentCity).html(response.name +"("+date+")" + "<img src="+iconurl+">");
+        
+ 
+         var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+         $(currentTemperature).html((tempF).toFixed(2)+"");
+         
+         $(currentHumidty).html(response.main.humidity+"%");
+         
+         var ws=response.wind.speed;
+         var windsmph=(ws*2.237).toFixed(1);
+         $(currentWSpeed).html(windsmph+"MPH");
+         
+         UVIndex(response.coord.lon,response.coord.lat);
+         forecast(response.id);
+         if(response.cod==200){
+             sCity=JSON.parse(localStorage.getItem("cityname"));
+             console.log(sCity);
+             if (sCity==null){
+                 sCity=[];
+                 sCity.push(city.toUpperCase()
+                 );
+                 localStorage.setItem("cityname",JSON.stringify(sCity));
+                 addToList(city);
+             }
+             else {
+                 if(find(city)>0){
+                     sCity.push(city.toUpperCase());
+                     localStorage.setItem("cityname",JSON.stringify(sCity));
+                     addToList(city);
+                 }
+             }
+         }
+ 
+     });
+ }
